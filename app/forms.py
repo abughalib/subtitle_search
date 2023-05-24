09e261f8325cd0fe9fc378d197fdb3319bcf62a7
abu <abughalib64@gmail.com>
2023-05-24 20:13:54 +0530

    small changes

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 
+
 class FileUploadForm(forms.Form):
     file = forms.FileField(required=True, label='Upload File')
+
 
 class SearchPhraseForm(forms.Form):
     search_phrase = forms.CharField(required=True, label='Search Phrase')
